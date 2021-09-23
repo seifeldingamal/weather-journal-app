3e8fdc5ff9e59cfb00e6d25506f6bc22e59d71c5
Seifeldin Abouelella <seifaldin.aboelella@gmail.com>
2021-09-23 12:20:02 +0200

    Deployment fixes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.static('website'));
 
 // Setup Server
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // Spin up the server
 const server = app.listen(port, listening);
